4aa3846b8fb9536544bda43836654d0cac19bce4
Wouter Laermans <wouter.laermans@appwise.be>
2024-03-14 16:10:36 +0100

    change `FormCheckbox` size

--- packages/components/src/components/checkbox/FormCheckbox.vue
+++ packages/components/src/components/checkbox/FormCheckbox.vue
@@ -46,18 +46,20 @@ const computedModel = computed<'indeterminate' | boolean>({
       'border-destructive focus-visible:ring-destructive data-[state=checked]:border-destructive data-[state=checked]:bg-destructive': props.isInvalid,
       'border-input-border focus-visible:ring-ring data-[state=checked]:border-primary data-[state=checked]:bg-primary': !props.isInvalid,
     }"
-    class="flex size-6 items-center justify-center rounded-input border border-solid outline-none ring-offset-2 ring-offset-background duration-200 focus-visible:ring-2 disabled:cursor-not-allowed disabled:opacity-50"
+    class="flex size-5 items-center justify-center rounded border-[1.5px] border-solid outline-none ring-offset-2 ring-offset-background duration-200 focus-visible:ring-2 disabled:cursor-not-allowed disabled:opacity-50"
   >
     <CheckboxIndicator>
       <AppIcon
         v-if="props.isIndeterminate"
         icon="minus"
+        size="sm"
         class="text-primary-foreground"
       />
 
       <AppIcon
         v-else-if="computedModel === true"
         icon="checkmark"
+        size="sm"
         class="text-primary-foreground"
       />
     </CheckboxIndicator>
