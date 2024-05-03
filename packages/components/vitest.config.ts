397cc39d892ef5e58464b5c4c7c09995eda7da84
kirylmaltsav <kiryl.maltsav@appwise.be>
2024-05-03 14:51:35 +0200

    Add tests for AppBadge

---
+++ packages/components/vitest.config.ts
@@ -0,0 +1,20 @@
+import { fileURLToPath } from 'node:url'
+
+import { configDefaults, defineConfig, mergeConfig } from 'vitest/config'
+
+import viteConfig from './vite.config'
+
+const URL = import.meta.url;
+
+export default defineConfig(() =>
+	mergeConfig(
+		viteConfig,
+		defineConfig({
+			test: {
+				environment: 'jsdom',
+				exclude: [...configDefaults.exclude],
+				root: fileURLToPath(new URL('./', URL)),
+			},
+		})
+	)
+)
