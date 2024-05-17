1aef3e8da406be16d9e240988a0617c5e5212a90
Wouter Laermans <wouter.laermans@appwise.be>
2024-05-17 15:22:26 +0200

    fix(AppTable): add `relative` class to table container
    - this fixes a bug where the empty state would float somewhere randomly because of incorrect positioning

--- packages/components/src/components/table/AppTable.vue
+++ packages/components/src/components/table/AppTable.vue
@@ -140,7 +140,7 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <div class="flex h-full flex-1 flex-col overflow-hidden rounded-xl border border-solid border-border bg-background">
+  <div class="relative flex h-full flex-1 flex-col overflow-hidden rounded-xl border border-solid border-border bg-background">
     <AppTableTop
       :is-loading="props.isLoading"
       :title="props.title"
