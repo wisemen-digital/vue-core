3f0c1989dd9707587ee826ffc20452d5baac4f3d
Robbe <robbevaes95@gmail.com>
2024-06-14 09:34:29 +0200

    feat: appDialog styles

--- packages/components/src/components/dialog/AppDialogContent.vue
+++ packages/components/src/components/dialog/AppDialogContent.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
 import { DialogContent } from 'radix-vue'
+import { computed } from 'vue'
 
 import AppDialogCloseButton from '@/components/dialog/AppDialogCloseButton.vue'
+import { useDialogStyle } from '@/components/dialog/dialog.style'
 
 const props = defineProps<{
   hideCloseButton: boolean
 }>()
 
+const dialogStyle = useDialogStyle()
+const contentClasses = computed<string>(() => dialogStyle.content())
+const closeButtonClasses = computed<string>(() => dialogStyle.closeButton())
+
 function onInteractOutside(e: CustomEvent): void {
   const target = e.target as HTMLElement
 
-  const isOverlay = target.classList.contains('dialog-overlay')
+  const isOverlay = target.classList.contains('custom-dialog-overlay')
 
   if (!isOverlay) {
     e.preventDefault()
@@ -26,7 +32,7 @@ function onOpenAutoFocus(e: Event): void {
   <DialogContent
     :force-mount="true"
     :disable-outside-pointer-events="false"
-    class="fixed left-1/2 top-1/2 z-dialog -translate-x-1/2 -translate-y-1/2 rounded-dialog bg-background shadow-dialog-shadow outline-none"
+    :class="contentClasses"
     @interact-outside="onInteractOutside"
     @open-auto-focus="onOpenAutoFocus"
   >
@@ -35,7 +41,7 @@ function onOpenAutoFocus(e: Event): void {
 
       <AppDialogCloseButton
         v-if="!props.hideCloseButton"
-        class="!absolute right-2 top-2"
+        :class="closeButtonClasses"
       />
     </div>
   </DialogContent>
