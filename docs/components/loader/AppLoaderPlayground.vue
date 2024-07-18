536ff0d1cba3a063dc646e336a91d0537cffba9d
Nick Banken <nick.banken@appwise.be>
2024-07-18 10:01:03 +0200

    feature: fix controls issue

--- docs/components/loader/AppLoaderPlayground.vue
+++ docs/components/loader/AppLoaderPlayground.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
 import ComponentPlayground from '@docs/playground/components/ComponentPlayground.vue'
-import { createControls } from '@docs/playground/utils/createContols'
 import { AppLoader } from '@wisemen/vue-core'
 
-const controls = createControls(null)
+const controls = null
 </script>
 
 <template>
