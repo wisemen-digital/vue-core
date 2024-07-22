556cc1c18718d54020c5b469f6c3848e202e108b
Tanya-Amber-L <tanya.leenders@gmail.com>
2024-07-22 09:51:20 +0200

    Pass AppButtons to auto-gen

--- packages/constant/components.ts
+++ packages/constant/components.ts
@@ -5,6 +5,11 @@ export const components = {
   breadcrumbs: [
     'AppBreadcrumbs',
   ],
+  button: [
+    'AppButton',
+    'AppIconButton',
+    'AppRouterLinkButton',
+  ],
   toast: [
     'AppToast',
   ]
