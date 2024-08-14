fa503218cf4efb22d5a7c3d62b4d7a000a441730
Wouter Laermans <wouter.laermans@appwise.be>
2024-08-14 11:32:19 +0200

    remove `TimePickerValue` type

--- packages/components/src/types/date.type.ts
+++ packages/components/src/types/date.type.ts
@@ -2,11 +2,6 @@ export interface MonthPickerValue {
   month: number | string
   year: number | string
 }
-export interface TimePickerValue {
-  hours: number | string
-  minutes: number | string
-  seconds?: number | string
-}
 
 export interface DatePickerRangeValue {
   end: Date | string
