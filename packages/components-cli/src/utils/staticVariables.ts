c37455f0e919568e785f473fd6d7423fc19ea387
Robbe <robbevaes95@gmail.com>
2024-09-13 10:16:35 +0200

    fix: correct api url

--- packages/components-cli/src/utils/staticVariables.ts
+++ packages/components-cli/src/utils/staticVariables.ts
@@ -1,2 +1,2 @@
-// export const baseUrl = process.env.COMPONENTS_BASE_URL ?? 'https://wisemen-digital.github.io/vue-core/_generation'
-export const baseUrl = process.env.COMPONENTS_BASE_URL ?? 'http://localhost:5173/vue-core/_generation'
+export const baseUrl = process.env.COMPONENTS_BASE_URL ?? 'https://wisemen-digital.github.io/vue-core/_generation'
+// export const baseUrl = process.env.COMPONENTS_BASE_URL ?? 'http://localhost:5173/vue-core/_generation'
