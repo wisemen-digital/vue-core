52f95ed168deaadc528871441d8e6324e63ec293
kirylmaltsav <kiryl.maltsav@appwise.be>
2024-10-02 16:15:47 +0200

    Fix bug where handleFilterChange doesn't clear keys that are undefined

--- packages/components/src/composables/pagination/pagination.composable.ts
+++ packages/components/src/composables/pagination/pagination.composable.ts
@@ -95,12 +95,20 @@ export function usePagination<TFilters>({
   }
 
   function handleFilterChange(event: FilterChangeEvent<TFilters>): void {
+    function removeUndefinedFilters(filters: PaginationFilters<TFilters>): PaginationFilters<TFilters> {
+      return Object.fromEntries(
+        Object.entries(filters).filter(([
+          , value,
+        ]) => value !== undefined),
+      ) as PaginationFilters<TFilters>
+    }
+
     paginationOptions.value = {
       ...paginationOptions.value,
-      filters: {
+      filters: removeUndefinedFilters({
         ...paginationOptions.value.filters,
         ...event,
-      } as PaginationFilters<TFilters>,
+      } as PaginationFilters<TFilters>),
       pagination: {
         ...paginationOptions.value.pagination,
         page: 0,
