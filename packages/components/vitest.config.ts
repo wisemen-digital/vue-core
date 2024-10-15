017bbf2e27e334e8760aa96c0acc0cb5c7ce7ecc
Robbe <robbevaes95@gmail.com>
2024-10-15 10:47:57 +0200

    fix: restore vitest

---
+++ packages/components/vitest.config.ts
@@ -0,0 +1,57 @@
+import { resolve } from 'node:path'
+
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+const projectRootDir = resolve(__dirname)
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      name: 'vue-core',
+      entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'index',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library (Vue)
+      external: [
+        'vue',
+        'vue-i18n',
+        'vue-router',
+        'zod',
+        'reka-ui',
+      ],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          'reka-ui': 'RekaUI',
+          'vue': 'Vue',
+          'vue-i18n': 'VueI18n',
+          'vue-router': 'VueRouter',
+          'zod': 'Zod',
+        },
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    dts({
+      cleanVueFileName: true,
+      exclude: [
+        'src/test/**',
+        'src/**/story/**',
+        'src/**/*.story.vue',
+      ],
+      tsconfigPath: 'tsconfig.build.json',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(projectRootDir, 'src'),
+    },
+  },
+})
