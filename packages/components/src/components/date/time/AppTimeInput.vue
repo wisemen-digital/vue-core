41d598a7b62cd68376ed4cc7c21f63a1b55e0c09
JeffreyNijs <109092992+JeffreyNijs@users.noreply.github.com>
2024-12-09 12:09:13 +0100

    feat: time input smart parse

--- packages/components/src/components/date/time/AppTimeInput.vue
+++ packages/components/src/components/date/time/AppTimeInput.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { vMaska } from 'maska/vue'
-
 import AppInput from '@/components/input/AppInput.vue'
 import type { Icon } from '@/icons/icons.js'
 
@@ -57,11 +55,34 @@ const modelValue = defineModel<string | null>({
   required: true,
 })
 
-function isValidTime(time: string): boolean {
-  // HH:MM
-  const timeRegex = /^(?:[01]?\d|2[0-3]):[0-5]\d$/
+function formatNumberToTime(time: number): string | null {
+  const timeString = time.toString()
 
-  return timeRegex.test(time)
+  if (time < 24) {
+    return `${timeString.padStart(2, '0')}:00`
+  }
+  else if (time < 60) {
+    return `00:${timeString.padStart(2, '0')}`
+  }
+  else if (time < 100) {
+    return `01:${(time - 60).toString().padStart(2, '0')}`
+  }
+  else if (timeString.length === 3) {
+    return `${timeString.slice(0, 1)}:${timeString.slice(1)}`
+  }
+  else if (timeString.length === 4) {
+    return `${timeString.slice(0, 2)}:${timeString.slice(2)}`
+  }
+
+  return null
+}
+
+function formatHoursMinutesToTime(hours: number, minutes: number): string | null {
+  if (hours < 24 && minutes < 60) {
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`
+  }
+
+  return null
 }
 
 function onInputBlur(): void {
@@ -69,9 +90,47 @@ function onInputBlur(): void {
     return
   }
 
-  const isValid = isValidTime(modelValue.value)
+  const time = modelValue.value
 
-  if (isValid) {
+  if (/^\d+$/.test(time) && time.length <= 4) {
+    modelValue.value = formatNumberToTime(Number.parseInt(time))
+
+    return
+  }
+
+  if (/^\d{1,2}h$/.test(time)) {
+    const hours: number = Number.parseInt(time.replace('h', ''))
+
+    modelValue.value = formatHoursMinutesToTime(hours, 0)
+
+    return
+  }
+
+  if (/^\d{1,2}m$/.test(time)) {
+    const minutes: number = Number.parseInt(time.replace('m', ''))
+
+    modelValue.value = formatHoursMinutesToTime(0, minutes)
+
+    return
+  }
+
+  if (/^\d{1,2}[hm]\d{1,2}m?$/.test(time)) {
+    const timeParts = time.split(/[hm]/)
+    const hours: number = Number.parseInt(timeParts[0] ?? '0')
+    const minutes: number = Number.parseInt(timeParts[1] ?? '0')
+
+    modelValue.value = formatHoursMinutesToTime(hours, minutes)
+
+    return
+  }
+
+  if (/^\d{1,2}[:.\-, ]\d{1,2}$/.test(time)) {
+    const timeParts = time.split(/[:.\-, ]/)
+    const hours: number = Number.parseInt(timeParts[0] ?? '0')
+    const minutes: number = Number.parseInt(timeParts[1] ?? '0')
+
+    modelValue.value = formatHoursMinutesToTime(hours, minutes)
+
     return
   }
 
@@ -90,7 +149,6 @@ function onInputBlur(): void {
     :placeholder="props.placeholder"
     :icon-left="props.iconLeft"
     :is-readonly="props.isReadonly"
-    v-maska="'##:##'"
     @blur="onInputBlur"
   >
     <template #right>
