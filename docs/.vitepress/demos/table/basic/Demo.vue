308e860199bcaffd99cd37f99ce65177a0e5d574
Tanya-Amber-L <tanya.leenders@gmail.com>
2025-02-13 11:52:15 +0100

    Add table docs & demo beginning

---
+++ docs/.vitepress/demos/table/basic/Demo.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { 
+  PaginatedData, 
+  TableColumn, 
+  VcTable, 
+  VcTableCell, 
+  usePagination 
+} from '@wisemen/vue-core'
+import { computed, h, VNode } from 'vue';
+
+interface Filters {}
+interface Index {
+  name: string
+}
+
+const pagination = usePagination<Filters>({
+  isRouteQueryEnabled: false,
+  key: 'table',
+  options: null,
+})
+
+const columns = computed<TableColumn<Index>[]>(() => [
+  {
+    cell: (row: Index): VNode => h(VcTableCell, null, { default: () => [
+      h('p', null, row.name),
+    ] }),
+    headerLabel: 'Name',
+    key: 'name',
+    width: '30rem',
+  },
+])
+
+const data: PaginatedData<Index> = {
+  data: [
+    { name: 'John Doe' },
+    { name: 'Jane Doe' },
+  ],
+  meta: {
+    total: 2,
+    limit: 10,
+    offset: 0,
+  },
+}
+</script>
+
+<template>
+  <VcTable
+    :pagination="pagination"
+    :is-loading="false"
+    :columns="columns"
+    :data="data"
+  >
+    <template #top>
+      <h4 class="p-md font-semibold">Users</h4>
+    </template>
+  </VcTable>
+</template>
---
+++ docs/components/table/table.md
@@ -0,0 +1,16 @@
+# Table 
+
+## Import
+
+```ts
+import { VcTable } from '@wisemen/vue-core'
+```
+
+<!-- @include: ./table-meta.md -->
+
+## Examples
+
+### Basic usage
+The most basic usage of the table component.
+
+<ComponentPreview name="table/basic" />
