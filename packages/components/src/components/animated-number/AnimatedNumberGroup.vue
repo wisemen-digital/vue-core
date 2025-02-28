3bbc71bc0c97a10b428b1580bcf781262c714d54
Wouter Laermans <wouter.laermans@appwise.be>
2025-02-28 17:15:02 +0100

    fix build

--- packages/components/src/components/animated-number/AnimatedNumberGroup.vue
+++ packages/components/src/components/animated-number/AnimatedNumberGroup.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import { NumberFlowGroup } from '@number-flow/vue'
+
 </script>
 
 <template>
-  <NumberFlowGroup>
-    <slot />
-  </NumberFlowGroup>
+  <div />
 </template>
