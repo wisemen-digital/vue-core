17c5d23b0938506f7307e482013620d6e8dd1b23
Wouter Laermans <wouter.laermans@appwise.be>
2025-03-12 20:50:04 +0100

    small refactor - rename `core` folder to `shared` - explicitly define props for `PrimitiveElement` and `InteractableElement`

--- packages/components/src/components/@next/shared/InteractableElement.vue
+++ packages/components/src/components/@next/shared/InteractableElement.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
 import { Primitive } from 'reka-ui'
 
-import type {
-  InteractableElement,
-} from '@/utils/props.util'
-
-const props = withDefaults(defineProps<InteractableElement & { as?: string | null }>(), {
+const props = withDefaults(defineProps<{
+  isDisabled: boolean
+  as?: string | null
+}>(), {
   isDisabled: false,
   as: null,
 })
