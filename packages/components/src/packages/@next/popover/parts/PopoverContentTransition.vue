cccb58477c0c073ac1ec0ded614fd0a61333518b
Wouter Laermans <wouter.laermans@appwise.be>
2025-03-15 19:22:57 +0100

    remove blur from popover content transition

--- packages/components/src/packages/@next/popover/parts/PopoverContentTransition.vue
+++ packages/components/src/packages/@next/popover/parts/PopoverContentTransition.vue
@@ -7,17 +7,14 @@ import { Motion } from 'motion-v'
     :initial="{
       opacity: 0,
       scale: 0,
-      filter: 'blur(4px)',
     }"
     :animate="{
       opacity: 1,
       scale: 1,
-      filter: 'blur(0px)',
     }"
     :exit="{
       opacity: 0,
       scale: 0,
-      filter: 'blur(4px)',
     }"
     :transition="{
       duration: 0.4,
