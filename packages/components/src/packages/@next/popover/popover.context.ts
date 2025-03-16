22d1337244555381073e3e503fa47b1086d56702
Wouter Laermans <wouter.laermans@appwise.be>
2025-03-16 18:02:02 +0100

    refactor provide / inject

--- packages/components/src/packages/@next/popover/popover.context.ts
+++ packages/components/src/packages/@next/popover/popover.context.ts
@@ -1,13 +1,11 @@
-import {
-  type ComputedRef,
-  inject,
-  type InjectionKey,
-  provide,
+import type {
+  ComputedRef,
 } from 'vue'
 
 import type { ClassConfig } from '@/customClassVariants'
 import type { PopoverProps } from '@/packages/@next/popover/popover.props'
 import type { CreatePopoverStyle } from '@/packages/@next/popover/style/popover.style'
+import { useContext } from '@/packages/@next/shared/context.composable'
 import type { PropsToComputed } from '@/utils/props.util'
 
 interface PopoverContext extends PropsToComputed<PopoverProps> {
@@ -16,18 +14,7 @@ interface PopoverContext extends PropsToComputed<PopoverProps> {
   style: ComputedRef<CreatePopoverStyle>
 }
 
-const popoverContextKey: InjectionKey<PopoverContext> = Symbol('popoverContext')
-
-export function useProvidePopoverContext(context: PopoverContext): void {
-  provide(popoverContextKey, context)
-}
-
-export function useInjectPopoverContext(): PopoverContext {
-  const context = inject(popoverContextKey)
-
-  if (context === undefined) {
-    throw new Error('Popover context is not provided. Make sure to wrap your components in `PopoverRoot`.')
-  }
-
-  return context
-}
+export const [
+  useProvidePopoverContext,
+  useInjectPopoverContext,
+] = useContext<PopoverContext>('popoverContext')
