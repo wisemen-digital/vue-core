1408336b75cb85e6436d29db06ca662831b5ae9e
Wouter Laermans <wouter.laermans@appwise.be>
2025-03-18 15:06:16 +0100

    improvements

--- packages/components/src/packages/@next/select/Select.vue
+++ packages/components/src/packages/@next/select/Select.vue
@@ -1,13 +1,11 @@
-<script setup lang="ts" generic="TValue extends SelectValue">
+<script setup lang="ts" generic="TValue extends SelectValueType">
 import SelectFilter from '@/packages/@next/select/parts/SelectFilter.vue'
 import SelectList from '@/packages/@next/select/parts/SelectList.vue'
 import SelectListItem from '@/packages/@next/select/parts/SelectListItem.vue'
 import SelectPopover from '@/packages/@next/select/parts/SelectPopover.vue'
 import SelectRoot from '@/packages/@next/select/parts/SelectRoot.vue'
-import SelectInlineFilter from '@/packages/@next/select/parts/value/SelectInlineFilter.vue'
-import SelectTagsValue from '@/packages/@next/select/parts/value/SelectTagsValue.vue'
-import SelectTextValue from '@/packages/@next/select/parts/value/SelectTextValue.vue'
-import type { SelectProps, SelectValue } from '@/packages/@next/select/select.props'
+import SelectValue from '@/packages/@next/select/parts/value/SelectValue.vue'
+import type { SelectProps, SelectValue as SelectValueType } from '@/packages/@next/select/select.props'
 
 const props = defineProps<SelectProps<TValue>>()
 
@@ -23,32 +21,30 @@ const modelValue = defineModel<TValue>({
   >
     <SelectPopover>
       <template #trigger>
-        <SelectInlineFilter v-if="false" />
-        <SelectTextValue v-if="true" />
-        <SelectTagsValue v-if="false" />
+        <SelectValue />
       </template>
 
       <template #content>
         <SelectFilter v-if="false" />
 
         <SelectList>
-          <SelectListItem value="1">
+          <SelectListItem value="First option">
             First option
           </SelectListItem>
 
-          <SelectListItem value="2">
+          <SelectListItem value="Second option">
             Second option
           </SelectListItem>
 
-          <SelectListItem value="3">
+          <SelectListItem value="Third option">
             Third option
           </SelectListItem>
 
-          <SelectListItem value="4">
+          <SelectListItem value="Fourth option">
             Fourth option
           </SelectListItem>
 
-          <SelectListItem value="5">
+          <SelectListItem value="Fifth option">
             Fifth option
           </SelectListItem>
         </SelectList>
---
+++ packages/components/src/packages/@next/select/parts/value/SelectValue.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import SelectPopoverTrigger from '@/packages/@next/select/parts/SelectPopoverTrigger.vue'
+import { useInjectSelectContext } from '@/packages/@next/select/select.context'
+
+const { modelValue } = useInjectSelectContext()
+</script>
+
+<template>
+  <div>
+    <span>
+      {{ modelValue }}
+    </span>
+
+    <SelectPopoverTrigger>
+      <button class="absolute inset-0" />
+    </SelectPopoverTrigger>
+  </div>
+</template>
