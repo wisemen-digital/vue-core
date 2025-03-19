678b59569eca9c04c846d1c4f4df42499bb27071
jordyblocken <blocken.jordy@hotmail.com>
2025-03-19 09:06:56 +0100

    added some test functionality

--- packages/components/src/packages/@next/button/button/tests/button.spec.ts
+++ packages/components/src/packages/@next/button/button/tests/button.spec.ts
@@ -6,8 +6,8 @@ import {
   vi,
 } from 'vitest'
 
-import type { ButtonProps } from '@/packages/@next/button/shared/button.props'
 import Button from '@/packages/@next/button/button/Button.vue'
+import type { ButtonProps } from '@/packages/@next/button/shared/button.props'
 
 describe('button', () => {
   /**
