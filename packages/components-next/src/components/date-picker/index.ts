13dfc99ca0b947fe181b0db43f40dc1afd5f55d7
Wouter Laermans <wouter.laermans@appwise.be>
2025-03-30 09:43:48 +0200

    wip(date-picker): export DatePicker components

--- packages/components-next/src/components/date-picker/index.ts
+++ packages/components-next/src/components/date-picker/index.ts
@@ -1,1 +1,4 @@
+export type { DatePickerProps as VcDatePickerProps } from '@/components/date-picker/single/datePicker.props'
 export { default as VcDatePicker } from '@/components/date-picker/single/DatePicker.vue'
+export { default as VcDatePickerDate } from '@/components/date-picker/single/parts/DatePickerDate.vue'
+export { default as VcDatePickerGrid } from '@/components/date-picker/single/parts/DatePickerGrid.vue'
