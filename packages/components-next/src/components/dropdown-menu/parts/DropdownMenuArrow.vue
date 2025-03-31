4ab5ebf48b5ccd820eb550660f3645c7781c8075
Wouter Laermans <wouter.laermans@appwise.be>
2025-03-31 08:56:04 +0200

    fix(dropdown-menu-arrow): fix name

--- packages/components-next/src/components/dropdown-menu/parts/DropdownMenuArrow.vue
+++ packages/components-next/src/components/dropdown-menu/parts/DropdownMenuArrow.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { DropdownMenuArrow as RekaDropdownMenuArraow } from 'reka-ui'
+import { DropdownMenuArrow as RekaDropdownMenuArrow } from 'reka-ui'
 
 import { useInjectDropdownMenuContext } from '@/components/dropdown-menu/dropdownMenu.context'
 import { mergeClasses } from '@/customClassVariants'
@@ -13,7 +13,7 @@ const {
 </script>
 
 <template>
-  <RekaDropdownMenuArraow
+  <RekaDropdownMenuArrow
     v-if="!isPopoverArrowHidden"
     as="div"
     class="relative -mb-4 h-4 w-10 overflow-hidden -translate-y-px"
@@ -23,5 +23,5 @@ const {
         class: mergeClasses(customClassConfig.arrow, classConfig?.arrow),
       })"
     />
-  </RekaDropdownMenuArraow>
+  </RekaDropdownMenuArrow>
 </template>
