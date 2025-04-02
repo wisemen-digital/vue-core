cbc6880844ff1145de5d5e5e65b21a233cfe4b3b
Jeroen Van Caekenberghe <jeroen.vancaekenberghe@appwise.be>
2025-04-02 13:58:43 +0200

    fix(type-error): context import

--- packages/components-next/src/components/switch/switch.context.ts
+++ packages/components-next/src/components/switch/switch.context.ts
@@ -2,7 +2,7 @@ import type { ComputedRef } from 'vue'
 
 import type { SwitchProps } from '@/components/switch/switch.props'
 import type { CreateSwitchStyle } from '@/components/switch/switch.style'
-import { useContext } from '@/composables/context.composable'
+import { useContext } from '@/composables/context/context.composable'
 import type { ClassConfig } from '@/customClassVariants'
 import type { PropsToComputed } from '@/utils/props.util'
 
