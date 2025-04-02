f503a8e70021af6ced7319256162741a5e4e7f1a
Wouter Laermans <wouter.laermans@appwise.be>
2025-04-02 09:03:37 +0200

    docs(keyboard-shortcut): add documentation for `keyboardKeys` prop

--- packages/components-next/src/components/keyboard-shortcut/keyboardShortcut.props.ts
+++ packages/components-next/src/components/keyboard-shortcut/keyboardShortcut.props.ts
@@ -1,5 +1,8 @@
 import type { KeyboardKey } from '@/types/keyboard.type'
 
 export interface KeyboardShortcutProps {
+  /**
+   * A list of keyboard keys that represent the shortcut combination.
+   */
   keyboardKeys: KeyboardKey[]
 }
