9083be8077d994830c64d30e1caf851bdacfc127
Wouter Laermans <wouter.laermans@appwise.be>
2025-04-06 10:24:15 +0200

    feat(RadioGroupItem): add `RadioGroupItemControl` component

---
+++ .changeset/small-dogs-nail.md
@@ -0,0 +1,5 @@
+---
+"@wisemen/vue-core-components": minor
+---
+
+Add `RadioGroupItemControl` component, which allows rendering the default radio indicator UI when overriding the default slot.
---
+++ packages/components-next/src/components/radio-group-item/parts/RadioGroupItemControl.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import RadioGroupItemIndicator from '@/components/radio-group-item/parts/RadioGroupItemIndicator.vue'
+import RadioGroupItemIndicatorTransition from '@/components/radio-group-item/parts/RadioGroupItemIndicatorTransition.vue'
+import { useInjectRadioGroupItemContext } from '@/components/radio-group-item/radioGroupItem.context'
+import { mergeClasses } from '@/customClassVariants'
+
+const {
+  classConfig,
+  customClassConfig,
+  style,
+} = useInjectRadioGroupItemContext()
+</script>
+
+<template>
+  <div
+    :class="style.control({
+      class: mergeClasses(classConfig?.control, customClassConfig?.control),
+    })"
+  >
+    <RadioGroupItemIndicator>
+      <RadioGroupItemIndicatorTransition />
+    </RadioGroupItemIndicator>
+  </div>
+</template>
