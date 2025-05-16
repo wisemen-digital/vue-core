087bba9efbe54aaf2d83918e2d1df899e57f2b7a
Wouter Laermans <wouter.laermans@appwise.be>
2025-05-16 13:08:22 +0200

    fix(Spinner): change styling to tailwind

---
+++ .changeset/deep-boats-clean.md
@@ -0,0 +1,5 @@
+---
+"@wisemen/vue-core-components": patch
+---
+
+Spinner: change styling to tailwind so it's not longer necessary to import the styling.
--- packages/components-next/src/components/spinner/Spinner.vue
+++ packages/components-next/src/components/spinner/Spinner.vue
@@ -2,52 +2,20 @@
   <svg
     viewBox="25 25 50 50"
     aria-hidden="true"
+    class="animate-spinner"
   >
     <circle
-      class="size-full"
+      class="animate-spinner-path size-full"
       cx="100%"
       cy="100%"
       fill="none"
       r="20"
       stroke="currentColor"
       stroke-miterlimit="10"
-      stroke-width="2"
+      stroke-width="4"
+      stroke-dasharray="1, 200"
+      stroke-dashoffset="0"
+      stroke-linecap="round"
     />
   </svg>
 </template>
-
-<style scoped>
-svg {
-  animation: loading-spinner-rotate 1s linear infinite;
-}
-svg circle {
-  stroke-dasharray: 1, 200;
-  stroke-dashoffset: 0;
-  animation:
-    loading-spinner-dash 2s ease-in-out infinite,
-    loading-spinner-color 8s ease-in-out infinite;
-  stroke-linecap: round;
-  stroke-width: 4px;
-}
-
-@keyframes loading-spinner-rotate {
-  100% {
-    transform: rotate(360deg);
-  }
-}
-
-@keyframes loading-spinner-dash {
-  0% {
-    stroke-dasharray: 1, 200;
-    stroke-dashoffset: 0;
-  }
-  50% {
-    stroke-dasharray: 89, 200;
-    stroke-dashoffset: -35px;
-  }
-  100% {
-    stroke-dasharray: 89, 200;
-    stroke-dashoffset: -124px;
-  }
-}
-</style>
