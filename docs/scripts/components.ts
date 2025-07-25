34eefe88a4786e1f6887cbe1c3ddc0f6659b333b
Tanya Leenders <tanya.leenders@appwise.be>
2025-07-25 14:20:36 +0200

    chore: fill up tooltip docs

--- docs/scripts/components.ts
+++ docs/scripts/components.ts
@@ -180,6 +180,7 @@ export const components: ComponentConfig[] = [
     sourceFolder: 'tooltip/parts/TooltipRoot.vue',
     targetFolder: 'tooltip',
     componentName: 'Tooltip',
+    styleFunctionName: 'createTooltipStyle',
   },
   // Dialog
   {
