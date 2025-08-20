27d1bcd74266e919cf5038e9e2ce3d9764e221cc
Jeroen Van Caekenberghe <jeroen.vancaekenberghe@appwise.be>
2025-08-20 16:10:35 +0200

    fix: naming of the exported  defineConfig

--- packages/open-api-plugins/src/error-code-enum/index.ts
+++ packages/open-api-plugins/src/error-code-enum/index.ts
@@ -1,4 +1,5 @@
 export {
-  defaultConfig, defineConfig,
+  defaultConfig as defaultErrorCodeEnumConfig,
+  defineConfig as defineErrorCodeEnumConfig,
 } from './config'
 export type { ErrorCodeEnumPlugin } from './types'
