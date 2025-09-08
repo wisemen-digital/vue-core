e112e2bb8f2a7dadf0e692bf6d9dfaf27d3c2f05
Wouter Laermans <wouter.laermans@appwise.be>
2025-09-08 14:58:46 +0200

    fix(Autocomplete): remove `filter` prop

--- packages/components-next/src/components/autocomplete/autocomplete.props.ts
+++ packages/components-next/src/components/autocomplete/autocomplete.props.ts
@@ -3,7 +3,7 @@ import type { AcceptableValue } from 'reka-ui'
 import type { SelectProps } from '@/components/select/select.props'
 import type { FormElement } from '@/utils/props.util'
 
-export interface AutocompleteProps<TValue extends AcceptableValue> extends Omit<SelectProps<TValue>, 'virtualList'>, FormElement {
+export interface AutocompleteProps<TValue extends AcceptableValue> extends Omit<SelectProps<TValue>, 'filter' | 'virtualList'>, FormElement {
   /**
    * Whether the search term is optional.
    * If `true`, the dropdown can be opened without entering a search term.
