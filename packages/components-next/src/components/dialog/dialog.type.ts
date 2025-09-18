369da9d8b3f63d844a9e7429a4542f99d09de6fb
Wouter Laermans <wouter.laermans@appwise.be>
2025-09-18 17:51:49 +0200

    chore: remove unused code

--- packages/components-next/src/components/dialog/dialog.type.ts
+++ packages/components-next/src/components/dialog/dialog.type.ts
@@ -1,10 +1,3 @@
-// /* eslint-disable style/operator-linebreak */
-// import type {
-//   Component,
-//   ComputedRef,
-//   Ref,
-// } from 'vue'
-
 import type { Component } from 'vue'
 
 export interface DialogTriggerProps {
@@ -37,56 +30,6 @@ type PickKeys<T extends object, TValue extends null | undefined> = NonNullable<
 type OptionalKeys<T extends object> = PickKeys<T, undefined>
 type RequiredKeys<T extends object> = Exclude<keyof T, OptionalKeys<T>>
 
-// export interface Constructor<P = never> {
-//   __isFragment?: never
-//   __isSuspense?: never
-//   __isTeleport?: never
-//   new (...args: never[]): { $props: P }
-// }
-
-// export type Attrs<TComponent> = Omit<
-//   {
-//     [K in keyof TComponent]: Ref<TComponent[K]> | TComponent[K]
-//   },
-//   'isOpen'
-// >
-
-// export interface UseDialogOptions<TComponent extends Component> {
-//   /**
-//    * The component to render. Must be a component of which the root is `AppDialog`
-//    */
-//   component: () => Promise<{ default: Constructor<TComponent> }>
-// }
-
-// export interface UseDialogReturnType<TComponent extends Component> {
-//   /**
-//    * Whether the dialog is open or not
-//    */
-//   isOpen: ComputedRef<boolean>
-//   /**
-//    * Close the dialog
-//    */
-//   close: () => void
-//   /**
-//    * Open the dialog
-//    * @param attrs The props to pass to the dialog - optional
-//    */
-//   open: Omit<Attrs<TComponent>, 'Symbol'> extends Record<string, never>
-//     ? // No params because there are no attributes
-//       () => Promise<void>
-//     : // Check if there are only optional attributes
-//     RequiredKeys<Omit<Attrs<TComponent>, 'Symbol'>> extends Record<string, never>
-//       ? // If there are only optional attributes, then the parameter is optional
-//         (attrs?: Omit<Attrs<TComponent>, IgnoredKeys>) => Promise<void>
-//       : // If there are required attributes, then the parameter is required
-//         (attrs: Omit<Attrs<TComponent>, IgnoredKeys>) => Promise<void>
-//   /**
-//    * The props to pass to the trigger
-//    * @param id The id of the dialog
-//    */
-//   triggerProps: ComputedRef<DialogTriggerProps>
-// }
-
 export type GetComponentProps<TComponent extends abstract new (...args: any) => any>
   = Omit<InstanceType<TComponent>['$props'], IgnoredProps>
 
@@ -101,7 +44,3 @@ export interface Dialog {
   isOpen: boolean
   component: Component
 }
-
-// export interface UseDialogContainerReturnType {
-//   dialogs: Ref<Dialog[]>
-// }
