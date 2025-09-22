511c7c42b8dc2574cbc02689bc6313e062c42b69
Robbe <robbevaes95@gmail.com>
2025-09-22 11:34:55 +0200

    chore: api utils with tsdown

--- packages/api-utils/vitest.config.ts
+++ packages/api-utils/vitest.config.ts
@@ -1,25 +1,26 @@
+import { resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 import {
   configDefaults,
   defineConfig,
-  mergeConfig,
 } from 'vitest/config'
 
-import viteConfig from './vite.config'
-
 const META_URL = import.meta.url
+const projectRootDir = resolve(__dirname)
 
-export default defineConfig(() =>
-  mergeConfig(
-    viteConfig,
-    defineConfig({
-      test: {
-        environment: 'jsdom',
-        exclude: [
-          ...configDefaults.exclude,
-        ],
-        root: fileURLToPath(new URL('./', META_URL)),
-      },
-    }),
-  ))
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(projectRootDir, 'src'),
+    },
+  },
+
+  test: {
+    environment: 'jsdom',
+    exclude: [
+      ...configDefaults.exclude,
+    ],
+    root: fileURLToPath(new URL('./', META_URL)),
+  },
+})
