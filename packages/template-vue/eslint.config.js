e0c3e81485eb3be5c2208b827b665c571b5f64af
Robbe <robbevaes95@gmail.com>
2025-09-26 14:40:19 +0200

    chore: added template packages

---
+++ packages/template-vue/eslint.config.js
@@ -0,0 +1,35 @@
+import eslintVueConfig from '@wisemen/eslint-config-vue'
+
+export default [
+  ...(await eslintVueConfig),
+  {
+    rules: {
+      '@intlify/vue-i18n/no-raw-text': 'off',
+      'project-structure/independent-modules': 'off',
+      'vue/no-undef-components': [
+        'error',
+        {
+          ignorePatterns: [
+            // Histoire components
+            'Story',
+            'Variant',
+          ],
+        },
+      ],
+      'vuejs-accessibility/form-control-has-label': 'off',
+      'vuejs-accessibility/label-has-for': 'off',
+    },
+  },
+  {
+    settings: {
+      'better-tailwindcss': {
+        entryPoint: 'src/styles/index.css',
+      },
+    },
+  },
+  {
+    rules: {
+      'unicorn/no-keyword-prefix': 'off',
+    },
+  },
+]
---
+++ packages/template-vue/src/index.ts
@@ -0,0 +1,2 @@
+export const EXAMPLE = 'example'
+export { default as TemplateComponent } from './Template.vue'
