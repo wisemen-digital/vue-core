1e491fb9f1ba73aa3f5b4d5f5cbf40c67f298774
Robbe <robbevaes95@gmail.com>
2025-09-27 01:48:06 +0200

    feat: easier doc navigation setup

---
+++ docs/packages/api-utils/apiUtils.navigation.ts
@@ -0,0 +1,13 @@
+import type { PackageDocNavigation } from '@docs/packages/navigation'
+
+export const API_UTILS_NAVIGATION: PackageDocNavigation = {
+  link: 'pages/index',
+  title: 'Api utils',
+  path: 'api-utils',
+  sidebar: [
+    {
+      text: `Api utils`,
+      link: 'pages/index',
+    },
+  ],
+}
--- docs/packages/components-v1/component-demos/button/loading/Demo.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { VcButton } from '@wisemen/vue-core'
-</script>
-
-<template>
-  <VcButton :is-loading="true">
-    Button
-  </VcButton>
-</template>
