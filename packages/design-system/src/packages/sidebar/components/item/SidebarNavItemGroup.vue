34fb0e6ad1620e19572a886175ba4afaadf68f7b
Wouter Laermans <wouter.laermans@appwise.be>
2026-01-13 08:44:00 +0100

    feat(design-system): sidebar [WIP]

---
+++ packages/design-system/src/packages/sidebar/components/item/SidebarNavItemGroup.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import ColumnLayout from '@/packages/column-layout/ColumnLayout.vue'
+import Text from '@/packages/text/Text.vue'
+
+const props = defineProps<{
+  label: string | null
+}>()
+</script>
+
+<template>
+  <ColumnLayout gap="sm">
+    <Text
+      v-if="props.label !== null"
+      :text="props.label"
+      class="block px-lg text-xs font-medium text-quaternary"
+    />
+
+    <ColumnLayout gap="xs">
+      <slot />
+    </ColumnLayout>
+  </ColumnLayout>
+</template>
