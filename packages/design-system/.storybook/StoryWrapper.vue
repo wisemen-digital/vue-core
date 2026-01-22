e2761a933551744b79bc8d5004684cd7e61f2eaa
Robbe <robbevaes95@gmail.com>
2026-01-22 12:46:47 +0100

    chore: workspace fixes

--- packages/design-system/.storybook/StoryWrapper.vue
+++ packages/design-system/.storybook/StoryWrapper.vue
@@ -1,8 +1,5 @@
-<script setup lang="ts">
-/* eslint-disable better-tailwindcss/no-unregistered-classes */
-</script>
-
 <template>
+  <!-- eslint-disable better-tailwindcss/no-unknown-classes -->
   <div
     class="
       default light flex items-center justify-center bg-primary p-4 text-primary
