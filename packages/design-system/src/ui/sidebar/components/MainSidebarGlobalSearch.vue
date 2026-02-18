d6ad7fccdf5452e69fe2688599a69d01e2bf9167
Jeroen Van Caekenberghe <jeroen-vc@Jeroens-MacBook-Pro.local>
2026-02-18 14:37:19 +0100

    fix(design-system): add click event on global search icon button in sidebar

--- packages/design-system/src/ui/sidebar/components/MainSidebarGlobalSearch.vue
+++ packages/design-system/src/ui/sidebar/components/MainSidebarGlobalSearch.vue
@@ -4,7 +4,14 @@ import { useI18n } from 'vue-i18n'
 
 import { UIIconButton } from '@/ui/button/index'
 
+const emit = defineEmits<{
+  click: []
+}>()
 const i18n = useI18n()
+
+function onClick(): void {
+  emit('click')
+}
 </script>
 
 <template>
@@ -12,5 +19,6 @@ const i18n = useI18n()
     :icon="SearchLgIcon"
     :label="i18n.t('components.sidebar.global_search.label')"
     variant="tertiary"
+    @click="onClick"
   />
 </template>
