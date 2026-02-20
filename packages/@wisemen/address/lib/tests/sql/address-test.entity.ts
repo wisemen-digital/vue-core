0c2cd971454dd8aa2a75118e7840a9ed6a80d33d
Kobe Kwanten <kobe.kwanten@wisemen.digital>
2026-02-20 14:07:19 +0100

    wip: migrate node packages

---
+++ packages/@wisemen/address/lib/tests/sql/address-test.entity.ts
@@ -0,0 +1,12 @@
+import { Entity, PrimaryColumn } from 'typeorm'
+import { AddressColumn } from '../../address-column.js'
+import { Address } from '../../address.js'
+
+@Entity()
+export class AddressTest {
+  @PrimaryColumn()
+  id: number
+
+  @AddressColumn({nullable: true})
+  address: Address | null
+}
