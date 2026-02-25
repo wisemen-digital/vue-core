2817b01633b2f6aee5977b0a07807737f57cb62c
Jeroen Van Caekenberghe <jeroen-vc@Jeroens-MacBook-Pro.local>
2026-02-25 15:34:44 +0100

    fix(design-system): value of numberbadge tos tring

--- packages/design-system/src/ui/number-badge/numberBadge.props.ts
+++ packages/design-system/src/ui/number-badge/numberBadge.props.ts
@@ -4,6 +4,9 @@ export interface NumberBadgeProps {
   ariaLabel?: string | null
   color?: NumberBadgeColor
   size?: 'lg' | 'md' | 'sm'
-  value: number
+  /*
+    * The number to display inside the badge. always use a formatted string here (e.g. 1.400, 4,5)
+  */
+  value: string
   variant?: 'outline' | 'solid' | 'translucent'
 }
