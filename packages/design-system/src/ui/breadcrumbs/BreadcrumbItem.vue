9b0c8d24de487bd36c6fb83bb7e711e9c27f2054
JeroenVanC <jeroen.vc1@icloud.com>
2026-03-04 15:10:34 +0100

    fix: breadcrumbs icon only

--- packages/design-system/src/ui/breadcrumbs/BreadcrumbItem.vue
+++ packages/design-system/src/ui/breadcrumbs/BreadcrumbItem.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
+import { twMerge } from 'tailwind-merge'
 import type { Component } from 'vue'
 import type { RouteLocationRaw } from 'vue-router'
 import { RouterLink } from 'vue-router'
 
+import ActionTooltip from '@/ui/action-tooltip/ActionTooltip.vue'
 import ClickableElement from '@/ui/clickable-element/ClickableElement.vue'
 import RowLayout from '@/ui/row-layout/RowLayout.vue'
 import { UIText } from '@/ui/text/index'
-import { twMerge } from '@/utils/twMerge.util'
 
 const props = defineProps<{
+  isLabelHidden?: boolean
   icon?: Component
-  label?: string
+  label: string
   to?: RouteLocationRaw
 }>()
 </script>
@@ -18,26 +20,36 @@ const props = defineProps<{
 <template>
   <Component
     :is="props.to !== undefined ? ClickableElement : 'span'"
+    class="min-w-0 overflow-hidden"
   >
     <Component
       :is="props.to !== undefined ? RouterLink : 'span'"
       :to="props.to"
-      class="group"
+      class="group min-w-0"
     >
-      <RowLayout gap="sm">
-        <Component
-          :is="props.icon"
-          v-if="props.icon"
-          :class="{
-            'group-hover:text-primary': props.to !== undefined,
-          }"
-          class="size-4 text-quaternary"
-        />
+      <RowLayout
+        class="min-w-0"
+        gap="sm"
+      >
+        <ActionTooltip
+          :is-disabled="!props.isLabelHidden"
+          :label="props.label"
+        >
+          <Component
+            :is="props.icon"
+            v-if="props.icon"
+            :class="{
+              'group-hover:text-primary': props.to !== undefined,
+            }"
+            class="size-4 shrink-0 text-quaternary"
+          />
+        </ActionTooltip>
         <UIText
           v-if="props.label"
           :text="props.label"
           :class="twMerge(
             'text-xs text-quaternary',
+            props.isLabelHidden && 'sr-only',
             props.to !== undefined ? `
               group-hover:text-primary group-hover:underline
             ` : '',
