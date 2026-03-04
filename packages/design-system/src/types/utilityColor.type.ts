48753e90a6901a4d01314991c144317421dcc5eb
JeroenVanC <jeroen.vc1@icloud.com>
2026-03-04 16:53:13 +0100

    fix(design-system): remove xxxs and smalles size of number badge

---
+++ packages/design-system/src/types/utilityColor.type.ts
@@ -0,0 +1,1 @@
+export type UtilityColor = 'blue' | 'brand' | 'error' | 'gray' | 'pink' | 'pink' | 'purple' | 'success' | 'warning'
--- packages/design-system/src/ui/number-badge/numberBadge.style.ts
+++ packages/design-system/src/ui/number-badge/numberBadge.style.ts
@@ -165,7 +165,7 @@ export const numberBadgeVariants = tv({
     {
       class: {
         base: `
-          bg-blue-25 border-blue-200
+          border-blue-200 bg-blue-25
           dark:border-blue-800 dark:bg-blue-950
         `,
         label: `
@@ -217,7 +217,7 @@ export const numberBadgeVariants = tv({
     {
       class: {
         base: `
-          bg-pink-25 border-pink-200
+          border-pink-200 bg-pink-25
           dark:border-pink-800 dark:bg-pink-950
         `,
         label: `
@@ -255,7 +255,7 @@ export const numberBadgeVariants = tv({
     {
       class: {
         base: `
-          bg-purple-25 border-purple-200
+          border-purple-200 bg-purple-25
           dark:border-purple-800 dark:bg-purple-950
         `,
         label: `
@@ -320,10 +320,6 @@ export const numberBadgeVariants = tv({
       md: {
         base: 'h-4 min-w-4 px-xxs',
         label: 'text-[0.625rem]',
-      },
-      sm: {
-        base: 'h-3.5 min-w-3.5 px-xxs',
-        label: 'text-xxxs',
       },
     },
 
