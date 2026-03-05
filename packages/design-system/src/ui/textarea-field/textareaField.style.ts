9a1d3ec7af3c4f8a16cd03bbaf1a125f39996331
JeroenVanC <jeroen.vc1@icloud.com>
2026-03-05 17:39:05 +0100

    fix(design-system): text area disabled cursor

--- packages/design-system/src/ui/textarea-field/textareaField.style.ts
+++ packages/design-system/src/ui/textarea-field/textareaField.style.ts
@@ -17,6 +17,7 @@ export const createTextareaFieldStyle = tv({
     textarea: `
       size-full bg-transparent text-xs text-primary outline-none
       placeholder:text-placeholder
+      read-only:cursor-default
       disabled:cursor-not-allowed disabled:text-disabled
       disabled:placeholder:text-fg-disabled-subtle
     `,
