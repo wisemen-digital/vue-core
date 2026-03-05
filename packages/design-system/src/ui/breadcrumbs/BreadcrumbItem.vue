553097551da460b92ab50f7bfe5f9f9b688e30e3
JeroenVanC <jeroen.vc1@icloud.com>
2026-03-05 16:36:54 +0100

    fix(breadcrumb): breadcrumbs trunxation with tooltip

--- packages/design-system/src/ui/breadcrumbs/BreadcrumbItem.vue
+++ packages/design-system/src/ui/breadcrumbs/BreadcrumbItem.vue
@@ -5,7 +5,6 @@ import { RouterLink } from 'vue-router'
 
 import ClickableElement from '@/ui/clickable-element/ClickableElement.vue'
 import RowLayout from '@/ui/row-layout/RowLayout.vue'
-import { UIActionTooltip } from '@/ui/action-tooltip/index'
 import { UIText } from '@/ui/text/index'
 import { twMerge } from '@/utils/twMerge.util'
 
@@ -17,40 +16,38 @@ const props = defineProps<{
 </script>
 
 <template>
-  <UIActionTooltip :label="props.label">
+  <Component
+    :is="props.to !== undefined ? ClickableElement : 'span'"
+    class="min-w-0 shrink overflow-hidden"
+  >
     <Component
-      :is="props.to !== undefined ? ClickableElement : 'span'"
-      class="min-w-0 shrink overflow-hidden"
+      :is="props.to !== undefined ? RouterLink : 'span'"
+      :to="props.to"
+      class="group min-w-0"
     >
-      <Component
-        :is="props.to !== undefined ? RouterLink : 'span'"
-        :to="props.to"
-        class="group min-w-0"
+      <RowLayout
+        class="min-w-0"
+        gap="sm"
       >
-        <RowLayout
-          class="min-w-0"
-          gap="sm"
-        >
-          <Component
-            :is="props.icon"
-            v-if="props.icon"
-            :class="{
-              'group-hover:text-primary': props.to !== undefined,
-            }"
-            class="size-4 shrink-0 text-quaternary"
-          />
-          <UIText
-            v-if="props.label"
-            :text="props.label"
-            :class="twMerge(
-              'text-xs text-quaternary',
-              props.to !== undefined ? `
-                group-hover:text-primary group-hover:underline
-              ` : '',
-            )"
-          />
-        </RowLayout>
-      </Component>
+        <Component
+          :is="props.icon"
+          v-if="props.icon"
+          :class="{
+            'group-hover:text-primary': props.to !== undefined,
+          }"
+          class="size-4 shrink-0 text-quaternary"
+        />
+        <UIText
+          v-if="props.label"
+          :text="props.label"
+          :class="twMerge(
+            'text-xs text-quaternary',
+            props.to !== undefined ? `
+              group-hover:text-primary group-hover:underline
+            ` : '',
+          )"
+        />
+      </RowLayout>
     </Component>
-  </UIActionTooltip>
+  </Component>
 </template>
