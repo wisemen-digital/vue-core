6d7b727a1efabc44550df5388d390adba9a23626
Robbe <robbevaes95@gmail.com>
2026-03-06 15:39:14 +0100

    feat: json sort and less generics

---
+++ packages/eslint-config/src/rules/json.ts
@@ -0,0 +1,38 @@
+import type { LintConfig } from '#types/lint.type.ts'
+
+export const jsonConfig: LintConfig[] = [
+  {
+  // Override rules for JSON files
+    files: [
+      '**/*.json',
+      '**/*.jsonc',
+      '**/*.json5',
+    ],
+    rules: {
+      'jsonc/sort-keys': [
+        'error',
+        {
+          order: {
+            type: 'asc',
+          },
+          pathPattern: '^$', // Validates the top-level keys
+        },
+        {
+          order: {
+            type: 'asc',
+          },
+          pathPattern: '.*', // Validates nested keys
+        },
+      ],
+    },
+  },
+  {
+  // Ignore package.json files from key sorting
+    files: [
+      '**/package.json',
+    ],
+    rules: {
+      'jsonc/sort-keys': 'off',
+    },
+  },
+]
