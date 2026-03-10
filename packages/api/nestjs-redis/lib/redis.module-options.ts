6fbe524647680af6080a5757af9c942e08cb9ca5
Sebastiaan Vanspauwen <43320258+SebastiaanVanspauwen@users.noreply.github.com>
2026-03-10 21:49:24 +0100

    feat: add redis module

---
+++ packages/api/nestjs-redis/lib/redis.module-options.ts
@@ -0,0 +1,8 @@
+import type { RedisClientOptions } from 'redis'
+
+export interface RedisModuleOptions extends RedisClientOptions {
+  url: string
+  onClientError: (error: unknown) => void
+  prefix?: string
+  ttl?: number
+}
