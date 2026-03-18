d44ccde10d2912d6e1fad0ce6088b3b419fcb7d8
JeroenVanC <jeroen.vc1@icloud.com>
2026-03-18 11:52:38 +0100

    fix(sidebar): simplify sidebar dimensions and remove pxToRem function

--- packages/design-system/src/ui/sidebar/mainSidebar.composable.ts
+++ packages/design-system/src/ui/sidebar/mainSidebar.composable.ts
@@ -10,22 +10,14 @@ import {
 import type { MainSidebarVariant } from '@/ui/sidebar/types/mainSidebar.type'
 
 const DEFAULT_SIDEBAR_WIDTH = '14rem'
-
-const SIDEBAR_COLLAPSED_WIDTH = 48
-const SIDEBAR_CONTAINER_PADDING = 9
-const SIDEBAR_ICON_SIZE = 16
-const SIDEBAR_ICON_CELL_SIZE = SIDEBAR_COLLAPSED_WIDTH - 2 * SIDEBAR_CONTAINER_PADDING
-const SIDEBAR_LOGO_SIZE = 24
-const SIDEBAR_AVATAR_SIZE = 24
-const SIDEBAR_LOGO_HEIGHT = 42
-
-function pxToRem(px: number): string {
-  const rootFontSize = Number.parseFloat(getComputedStyle(document.documentElement).fontSize)
-
-  return `${px / rootFontSize}rem`
-}
-
-const ICONS_ONLY_SIDEBAR_WIDTH = pxToRem(SIDEBAR_COLLAPSED_WIDTH)
+const ICONS_ONLY_SIDEBAR_WIDTH = '3rem' // 48px
+const SIDEBAR_CONTAINER_PADDING = '0.5625rem' // 9px
+const SIDEBAR_ICON_SIZE = '1rem' // 16px
+const SIDEBAR_ICON_CELL_SIZE = '1.875rem' // 30px (48 - 2 * 9)
+const SIDEBAR_LINK_HEIGHT = '1.875rem' // 30px
+const SIDEBAR_LOGO_PADDING = '0.1875rem' // 3px ((SIDEBAR_ICON_CELL_SIZE - 24) / 2)
+const SIDEBAR_AVATAR_PADDING = '0.1875rem' // 3px ((SIDEBAR_ICON_CELL_SIZE - 24) / 2)
+const SIDEBAR_LOGO_HEIGHT = '2.625rem' // 42px
 
 const isFloatingSidebarOpen = ref<boolean>(false)
 const sidebarWidth = ref<string>(DEFAULT_SIDEBAR_WIDTH)
@@ -62,13 +54,13 @@ export function useMainSidebar() {
     },
   })
 
-  const sidebarContainerPadding = pxToRem(SIDEBAR_CONTAINER_PADDING)
-  const sidebarIconSize = pxToRem(SIDEBAR_ICON_SIZE)
-  const sidebarIconCellSize = pxToRem(SIDEBAR_ICON_CELL_SIZE)
-  const sidebarLinkHeight = pxToRem(SIDEBAR_ICON_CELL_SIZE)
-  const sidebarLogoPadding = pxToRem((SIDEBAR_ICON_CELL_SIZE - SIDEBAR_LOGO_SIZE) / 2)
-  const sidebarAvatarPadding = pxToRem((SIDEBAR_ICON_CELL_SIZE - SIDEBAR_AVATAR_SIZE) / 2)
-  const sidebarLogoHeight = pxToRem(SIDEBAR_LOGO_HEIGHT)
+  const sidebarContainerPadding = SIDEBAR_CONTAINER_PADDING
+  const sidebarIconSize = SIDEBAR_ICON_SIZE
+  const sidebarIconCellSize = SIDEBAR_ICON_CELL_SIZE
+  const sidebarLinkHeight = SIDEBAR_LINK_HEIGHT
+  const sidebarLogoPadding = SIDEBAR_LOGO_PADDING
+  const sidebarAvatarPadding = SIDEBAR_AVATAR_PADDING
+  const sidebarLogoHeight = SIDEBAR_LOGO_HEIGHT
 
   function closeIfFloatingSidebar(): void {
     if (isFloatingSidebar.value) {
